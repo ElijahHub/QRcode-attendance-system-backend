541abfd25c8549c402960179e013370a5ad38d01
Elijah <elijahadewunmi2005@gmail.com>
2025-04-24 02:13:45 +0100

    add service function to retrive attendance reecord in attendance module

--- src/modules/attendance/attendance.service.ts
+++ src/modules/attendance/attendance.service.ts
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import prisma from "../../utils/prisma";
 import { CreateAttendanceInput } from "./attendance.schema";
 
@@ -21,4 +22,61 @@ export async function findSessionById(sessionId: string) {
   return await prisma.lectureSessions.findUnique({
     where: { id: sessionId },
   });
+}
+
+export async function getStudentAttendanceForSession(sessionId: string) {
+  const attendance = await prisma.attendanceRecords.findMany({
+    where: { sessionId },
+    include: { student: true },
+  });
+
+  return _.map(attendance, (record) => ({
+    date: record.timestamp,
+    matNumber: record.student.matNumber,
+    name: record.student.name,
+  }));
+}
+
+export async function getAllStudentAttendance(courseId: string) {
+  const sessions = await prisma.lectureSessions.findMany({
+    where: { courseId },
+    include: { attendance: { include: { student: true } } },
+  });
+
+  const allAttendance = _.flatMap(sessions, (session) =>
+    _.map(session.attendance, (record) => ({
+      date: record.timestamp,
+      matNumber: record.student.matNumber,
+      name: record.student.name,
+    }))
+  );
+
+  return allAttendance;
+}
+
+export async function getAttendanceForAStudent({
+  studentId,
+  courseId,
+}: {
+  studentId: string;
+  courseId: string;
+}) {
+  const courseSessions = await prisma.lectureSessions.count({
+    where: { courseId },
+  });
+
+  const attendedSessions = await prisma.attendanceRecords.count({
+    where: {
+      studentId,
+      lecture: {
+        courseId,
+      },
+    },
+  });
+
+  return {
+    total: courseSessions,
+    attended: attendedSessions,
+    status: `${attendedSessions}/${courseSessions}`,
+  };
 }
