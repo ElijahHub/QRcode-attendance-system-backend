885000e7032be690d0188881a00feac3b1b3b2a8
Elijah <elijahadewunmi2005@gmail.com>
2025-05-13 09:20:19 +0100

    setup for deployment

--- prisma/migrations/20250422135619_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `emailMac` to the `PasswordReset` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `passwordreset` ADD COLUMN `emailMac` VARCHAR(191) NOT NULL;
